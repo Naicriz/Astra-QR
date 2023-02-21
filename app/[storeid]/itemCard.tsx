ae59d8f9a369cc0cb2c83a013262526d7b9ef6d0
Naicriz <ian.dev@buzon.cl>
2023-02-21 03:59:17 -0300

    ➕ Added .idea and some base structure

---
+++ app/[storeid]/itemCard.tsx
@@ -0,0 +1,9 @@
+export default function ItemCard() {
+    return (
+            <div>
+                <h2 className="border border-red-300">
+                    Componente Tarjeta producto-item
+                </h2>
+            </div>
+            );
+}
